865424091967a5360fa47d95e3521290306b0656
miandoli <miandoli8@gmail.com>
2021-02-14 17:37:15 -0500

    Added a script to get the IPs used in the flow data that isn't in the inventory list

---
+++ find_unknown_ips.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+flow_data = pd.read_csv('sample_data/sample_data.csv', header=0)
+ip_data = pd.read_csv('sample_data/ip_inventory.csv', header=0, index_col=0)
+
+def find_unknowns(address_name, unknowns):
+    for ip in flow_data[address_name]:
+        try:
+            ip_data.at[ip, col_name]
+        except:
+            if ip in unknowns:
+                unknowns[ip] = unknowns[ip] + 1
+            else:
+                unknowns[ip] = 1
+
+unknown_ips = {}
+find_unknowns('Address A', unknown_ips)
+find_unknowns('Address B', unknown_ips)
+
+output_data = []
+for k, v in unknown_ips.items():
+    output_data.append([k, v])
+output_data = pd.DataFrame(output_data, columns=['IP Address', 'Occurrences'])
+output_data = output_data.sort_values(by=['Occurrences'], ascending=False)
+output_data.to_csv('output/unknown_ips.csv', index=False)
